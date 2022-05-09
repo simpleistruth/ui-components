62c44a87da9cec60254ac2a4dd9cec565f361c6c
simpleistruth <chen.j.anthony@gmail.com>
2022-05-08 23:19:56 -0700

    some more changes to setup proper dev environment

---
+++ components/test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>Hey this is the test :)</div>;
+};
+
+export default Test;
---
+++ pages/dropdown/index.tsx
@@ -0,0 +1,7 @@
+import styles from '@styles/Dropdown.module.scss';
+
+const DropDown = () => {
+  return <div>Hey there this is my dropdown menu</div>;
+};
+
+export default DropDown;
